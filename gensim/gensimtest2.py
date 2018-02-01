c12245f79568a8dbe9534b6e5c56a6ad6606254b
illool <32729865+illool@users.noreply.github.com>
2018-02-01 17:33:34 +0800

    Update gensimtest2.py

--- gensim/gensimtest2.py
+++ gensim/gensimtest2.py
@@ -1,52 +1,109 @@
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+#coding=utf-8
 from gensim import corpora, models, similarities
+import gensim
 
-dictionary = corpora.Dictionary.load('deerwester.dict')
-corpus = corpora.MmCorpus('deerwester.mm')
+import logging  
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+documents = ["Human machine interface for lab abc computer applications",  
+              "A survey of user opinion of computer system response time",  
+              "The EPS user interface management system",  
+              "System and human system engineering testing of EPS",  
+              "Relation of user perceived response time to error measurement",  
+              "The generation of random binary unordered trees",  
+              "The intersection graph of paths in trees",  
+              "Graph minors IV Widths of trees and well quasi ordering",  
+              "Graph minors A survey"]  
+
+# 去除停用词并分词  
+# 译者注：这里只是例子，实际上还有其他停用词  
+# 处理中文时，请借助 Py结巴分词 https://github.com/fxsjy/jieba  
+stoplist = set('for a of the and to in'.split())  
+texts = [[word for word in document.lower().split() if word not in stoplist]  for document in documents]  
+  
+# 去除仅出现一次的单词  
+from collections import defaultdict  
+frequency = defaultdict(int)
+for text in texts:  
+    for token in text:  
+        frequency[token] += 1  
+ 
+texts = [[token for token in text if frequency[token] > 1]  
+         for text in texts]  
+ 
+from pprint import pprint   # pretty-printer  
+pprint(texts) 
+
+dictionary = corpora.Dictionary(texts)
+dictionary.save('deerwester.dict') # 把字典保存起来，方便以后使用
+print(dictionary)
+#此时又12个不同的单词
+print(dictionary.token2id)
+
+new_doc = "Human computer interaction"
+#将new_doc分词后使用dictionary(字典)中的词来计数，输出的词为每个词对应dictionary(字典)中的id和在new_doc中出现的次数
+new_vec = dictionary.doc2bow(new_doc.lower().split())
+#computer”(id 0) 和“human”(id 1)各出现一次
+print(new_vec) # "interaction"没有在dictionary中出现，因此忽略 
+
+corpus = [dictionary.doc2bow(text) for text in texts] 
+corpora.MmCorpus.serialize('deerwester.mm', corpus) # 存入硬盘，以备后需  
 print(corpus)
+
+#读取文本的方式生成语料库
+class MyCorpus(object):
+    def __iter__(self):
+        for line in open('mycorpus.txt'):
+            # assume there's one document per line, tokens separated by whitespace
+            yield dictionary.doc2bow(line.lower().split())
+
+corpus_memory_friendly = MyCorpus() # 没有将整个语料库载入内存
+print(corpus_memory_friendly) 
+
+for vector in corpus_memory_friendly:
+    print(vector) 
+    
+# 收集所有符号的统计信息  
+dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
+# 收集停用词和仅出现一次的词的id 
+stop_ids = [dictionary.token2id[stopword] for stopword in stoplist  
+            if stopword in dictionary.token2id]  
+#Python3.5中：iteritems变为items
+## 得到仅出现一次的词
+once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq == 1]  
+print("once_ids",once_ids)
+dictionary.filter_tokens(stop_ids + once_ids) # 删除停用词和仅出现一次的词  
+print(dictionary.token2id)  
+dictionary.compactify() # 消除id序列在删除词后产生的不连续的缺口  
+print(dictionary.token2id)  
+
+from gensim import corpora  
+# 创建一个玩具级的语料库  
+corpus = [[(1, 0.5)], []]  # 让一个文档为空，作为它的heck
+#将corpus写入到corpus.mm
+corpora.MmCorpus.serialize('corpus.mm', corpus)
+#保存为不同的格式
+corpora.SvmLightCorpus.serialize('corpus.svmlight', corpus)  
+corpora.BleiCorpus.serialize('corpus.lda-c', corpus)  
+corpora.LowCorpus.serialize('corpus.low', corpus) 
+
+#读取一个.mm文件
+corpus = corpora.MmCorpus('corpus.mm')  
+print(corpus)
+# 将语料库全部导入内存的方法 
+# 调用list()将会把所有的序列转换为普通Python List 
 print(list(corpus))
-
-# 文档从一种向量表示方式转换到另一种
-# 将语料库中隐藏的结构发掘出来，发现词语之间的关系，并且利用这些结构、关系使用一种新的、更有语义价值的（这是我们最希望的）方式描述其中的文档。
-# 使得表示方式更加简洁。这样不仅能提高效率（新的表示方法一般消耗较少的资源）还能提高效果（忽略了边际数据趋势、降低了噪音）。
-
-# 我们使用了前一个教程中用过的语料库来初始化（训练）这个转换模型。不同的转换可能需要不同的初始化参数；在Tfidf案例中，“训练”仅仅是遍历提供的语料库然后计算所有属性的文档频率（译者注：在多少文档中过）。训练其他模型，例如潜在语义分析或隐含狄利克雷分配，更加复杂，因此耗时也多
-# 第一步 -- 初始化一个模型
-tfidf = models.TfidfModel(corpus)
-
-doc_bow = [(0, 1), (1, 2)]
-print(tfidf[doc_bow])# 第二步 -- 使用模型转换向量
-
-corpus_tfidf = tfidf[corpus]
-for doc in corpus_tfidf:
-    print(doc)
+# 另一种利用流接口，一次只打印一个文档  
+for doc in corpus:  
+    print(doc)  
     
-    
-lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=2) # 初始化一个LSI转换
-corpus_lsi = lsi[corpus_tfidf] # 在原始语料库上加上双重包装: bow->tfidf->fold-in-lsi   
-lsi.print_topics(2) 
+ #想将这个 Matrix Market格式的语料库存为Blei’s LDA-C格式   
+corpora.BleiCorpus.serialize('corpus.lda-c', corpus)  
 
-for doc in corpus_lsi:
-    print(doc)
-    
-    
-lsi.save('model.lsi') # same for tfidf, lda, ...
-lsi = models.LsiModel.load('model.lsi')
+#Gensim包含了许多高效的工具函数来帮你实现语料库与numpy矩阵之间互相转换
+#corpus = gensim.matutils.Dense2Corpus(numpy_matrix)
+#numpy_matrix = gensim.matutils.corpus2dense(corpus, num_terms=number_of_corpus_features) 
+#以及语料库与scipy稀疏矩阵之间的转换：
+#corpus = gensim.matutils.Sparse2Corpus(scipy_sparse_matrix)
+scipy_csc_matrix = gensim.matutils.corpus2csc(corpus) 
 
-#Gensim实现了几种常见的向量空间模型算法：
-
-#词频-逆文档频（Term Frequency * Inverse Document Frequency， Tf-Idf）
-#model = tfidfmodel.TfidfModel(bow_corpus, normalize=True)
-
-#潜在语义索引（Latent Semantic Indexing，LSI，or sometimes LSA）
-#model = lsimodel.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=300)
-
-#随机映射（Random Projections，RP）
-#model = rpmodel.RpModel(tfidf_corpus, num_topics=500)
-
-#隐含狄利克雷分配（Latent Dirichlet Allocation, LDA）
-#model = ldamodel.LdaModel(bow_corpus, id2word=dictionary, num_topics=100)
-
-#分层狄利克雷过程（Hierarchical Dirichlet Process，HDP）
-#model = hdpmodel.HdpModel(bow_corpus, id2word=dictionary)

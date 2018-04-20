0cd6313d697833676319559b9f2b75538e8bfa0d
illool <32729865+illool@users.noreply.github.com>
2018-04-20 14:33:36 +0800

    Update SVM.py

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -12,22 +12,25 @@ import random
 import numpy as np
 import math
 import copy
-a=np.matrix([[1.2,3.1,3.1]])
-#print a.astype(int)
-#print a.A
+#a=np.matrix([[1.2,3.1,3.1]])
+#print(a.astype(int))
+#print(a.A)
 
 class SVM:
       def __init__(self,data,kernel,maxIter,C,epsilon):
             self.trainData=data
             self.C=C  #惩罚因子
             self.kernel=kernel
-            self.maxIter=maxIter
-            self.epsilon=epsilon
-            self.a=[0 for i in range(len(self.trainData))]
-            self.w=[0 for i in range(len(self.trainData[0][0]))]
-            self.eCache=[[0,0] for i in range(len(self.trainData))]
+            self.maxIter=maxIter #最大迭代次数 
+            self.epsilon=epsilon #结果的精确性 
+            #a中部等于零，对应到train_data就是支持向量
+            self.a=[0 for i in range(len(self.trainData))]#用来存放a_i
+            self.w=[0 for i in range(len(self.trainData[0][0]))]#参数的权重
+            self.eCache=[[0,0] for i in range(len(self.trainData))]#放着Ei，表示预测值与真实值之差为
             self.b=0
+            #样本数据xL
             self.xL=[self.trainData[i][0] for i in range(len(self.trainData))]
+            #样本结果yL
             self.yL=[self.trainData[i][1] for i in range(len(self.trainData))]
 
       def train(self):
@@ -44,6 +47,7 @@ class SVM:
                   K=0
                   for m in range(len(A)):
                        K+=(A[m]-B[m])**2 
+                       #print(A[m],B[m],self.kernel[1])
                   res=math.exp(-0.5*K/(self.kernel[1]**2))
             return res
 
@@ -59,11 +63,15 @@ class SVM:
             #SMO是基于 KKT 条件的迭代求解最优化问题算法
             #SMO是SVM的核心算法
             support_Vector=[]
-            self.a=[0 for i in range(len(self.trainData))]
+            #初始化的时候已经设置过了可一干掉
+            #self.a=[0 for i in range(len(self.trainData))]
+            #将a_older拷贝出来，后面更新要用
             pre_a=copy.deepcopy(self.a)
+            #迭代计算
             for it in range(self.maxIter):
+                #flag用于判断时候达到计算精度的要求
                   flag=1
-                  for i in range(len(self.xL)):
+                  for i in range(len(self.xL)):#样本数据xL
                         #print self.a
                         #更新 self.a  使用 机器学习实战的求解思路
                         #计算 j更新
@@ -73,30 +81,37 @@ class SVM:
                         #机器学习实战 硬币书表述正常 代码混乱且有错误 启发式搜索
                         Ei=self.__calE(self.xL[i],self.yL[i])
                         j,Ej=self.__chooseJ(i,Ei)
-                        #计算 L H
+                        #计算 L H,aj的可行域
                         (L,H)=self.__calLH(pre_a,j,i)
                         #思路是先表示为self.a[j] 的唯一变量的函数 再进行求导（一阶导数=0 更新）
-                        kij=self.__kernel(self.xL[i],self.xL[i])+self.__kernel(self.xL[j],self.xL[j])-2*self.__kernel(self.xL[i],self.xL[j])
+                        #kij = k(ii)+k(jj)-2*k(ij),其中k是核函数
+                        #kij
+                        kij=self.__kernel(self.xL[i],self.xL[i]) + self.__kernel(self.xL[j],self.xL[j]) - 2*self.__kernel(self.xL[i],self.xL[j])
                         #print kij,"aa"
-                        if(kij==0):
+                        if(kij==0): #分母不能为0，为0放弃计算
                               continue
+                        #计算aj_new，pre_a[j]是a_old
+                        #aj_new = a_old+y(j)*(Ei-Ej)/k(ii)+k(jj)-2*k(ij)
                         self.a[j] = pre_a[j] + float(1.0*self.yL[j]*(Ei-Ej))/kij
                         #下届是L 也就是截距,小于0时为0
                         #上届是H 也就是最大值,大于H时为H
+                        #更新算出来的aj_new
                         self.a[j] = min(self.a[j], H)
                         self.a[j] = max(self.a[j], L)
                         #self.a[j] = min(self.a[j], H)
                         #print L,H
                         self.eCache[j]=[1,self.__calE(self.xL[j],self.yL[j])]
+                        #计算ai_new = ai_older + yL[i]*yL[j]*((aj_older-aj_new)
                         self.a[i] = pre_a[i]+self.yL[i]*self.yL[j]*(pre_a[j]-self.a[j])
                         self.eCache[i]=[1,self.__calE(self.xL[i],self.yL[i])]
+                        #计算误差
                         diff=sum([abs(pre_a[m]-self.a[m]) for m in range(len(self.a))])
                         #print diff,pre_a,self.a
                         if diff < self.epsilon:
                               flag=0
                         pre_a=copy.deepcopy(self.a)
                   if flag==0:
-                        print it,"break"
+                        print(it,"break")
                         break
 
             #return support_Vector
@@ -123,37 +138,45 @@ class SVM:
                               Ej=Ek
                   return j,Ej
             else:
-                  #最初始状态
+                  #最初始状态״̬
                   j=self.__randJ(i)
                   Ej=self.__calE(self.xL[j],self.yL[j])
                   return j,Ej
 
-      def __randJ(self,i):
+      def __randJ(self,i):#随机选择一个j出来,j的范围为[0,len(self.xL)-1]
             j=i
             while(j==i):
                   j=random.randint(0,len(self.xL)-1)
             return j
-
+      #可行域选择
       def __calLH(self,pre_a,j,i):
             if(self.yL[j]!= self.yL[i]):
+                #可行域为L = max[0,a2-a1];H = min[C,C+a2-a1]
                   return (max(0,pre_a[j]-pre_a[i]),min(self.C,self.C-pre_a[i]+pre_a[j]))
             else:
+                #可行域为L = max[0,a2+a1-C];H = min[C,a2+a1]
                   return (max(0,-self.C+pre_a[i]+pre_a[j]),min(self.C,pre_a[i]+pre_a[j]))
-
+              
+      #Ei表示预测值与真实值之差为
       def __calE(self,x,y):
             #print x,y
+            #真实值-预测值
             y_,q=self.predict(x)
             return y_-y
 
       def __calW(self):
-            self.w=[0 for i in range(len(self.trainData[0][0]))]
+            #print(len(self.trainData[0][0]))
+            #给参数的权重赋值，在初始化的时候已经设置过了可以干掉
+            #self.w=[0 for i in range(len(self.trainData[0][0]))]
             for i in range(len(self.trainData)):
                   for j in range(len(self.w)):
+                        #w=∑i=1->m αi*yi*xi
+                        print(self.w[j],self.a[i],self.yL[i],self.xL[i][j])
                         self.w[j]+=self.a[i]*self.yL[i]*self.xL[i][j]
 
       def __update(self):
             #更新 self.b 和 self.w
-            self.__calW()
+            self.__calW()#更新 self.w
             #得到了self.w 下面求b
             #print self.a
             maxf1=-99999
@@ -173,6 +196,7 @@ class SVM:
             pre_value=0
             #从trainData 改成 suport_Vector
             for i in range(len(self.trainData)):
+                  #decision rule 
                   pre_value+=self.a[i]*self.yL[i]*self.__kernel(self.xL[i],testData)
             pre_value+=self.b
             #print pre_value,"pre_value"

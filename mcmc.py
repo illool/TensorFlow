d134f7760612d4d71e18d0fda4f75fdcc7a19eda
illool <32729865+illool@users.noreply.github.com>
2018-04-27 18:27:40 +0800

    Update mcmc.py

--- mcmc.py
+++ mcmc.py
@@ -1,5 +1,4 @@
 #coding=utf-8
-#https://www.cnblogs.com/pinard/p/6638955.html
 import random
 import math
 from scipy.stats import norm
@@ -26,6 +25,7 @@ while t < T-1:
         pi[t] = pi[t - 1]
 
 
+print("在例子里，我们的目标平稳分布是一个均值3，标准差2的正态分布，\n而选择的马尔可夫链状态转移矩阵Q(i,j)的条件转移概率是以i为均值,方差1的正态分布在位置j的值。\n这个例子仅仅用来让大家加深对M-H采样过程的理解。毕竟一个普通的一维正态分布用不着去用M-H采样来获得样本。")
 plt.scatter(pi, norm.pdf(pi, loc=3, scale=2))
 num_bins = 50
 plt.hist(pi, num_bins, normed=1, facecolor='red', alpha=0.7)

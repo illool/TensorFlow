cb9b537d26528564e53cdba05001ea3c1016c5cd
illool <32729865+illool@users.noreply.github.com>
2018-05-02 19:44:33 +0800

    Update mcmc_gibbs.py

--- mcmc_gibbs.py
+++ mcmc_gibbs.py
@@ -5,17 +5,24 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.stats import multivariate_normal
 #多元正态分布，方差和协方差
+#cov:
+#σ21   ρσ1σ2 
+#ρσ1σ2 σ22
 samplesource = multivariate_normal(mean=[5,-1], cov=[[1,0.5],[0.5,2]])
 
 def p_ygivenx(x, m1, m2, s1, s2):
     #得到正态分布的随机数,均值，方差
+    #每一变量的条件概率为
+    #P(x2|x1)=Norm(μ2+ρσ2/σ1(x1−μ1),(1−ρ2)σ22)
     return (random.normalvariate(m2 + rho * s2 / s1 * (x - m1), math.sqrt(1 - rho ** 2) * s2))
 
 def p_xgiveny(y, m1, m2, s1, s2):
     #得到正态分布的随机数,均值，方差
+    #每一变量的条件概率为
+    #P(x1|x2)=Norm(μ1+ρσ1/σ2(x2−μ2),(1−ρ2)σ21)
     return (random.normalvariate(m1 + rho * s1 / s2 * (y - m2), math.sqrt(1 - rho ** 2) * s1))
 
-N = 50000
+N = 5000
 K = 20
 x_res = []
 y_res = []
